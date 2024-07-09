23a0e6486b712f0068f0b45e3d4556009c2a7791
Ann Kh <anna.khrust@gmail.com>
2024-07-09 09:58:56 +0300

    task 2 completed

--- src/js/app.js
+++ src/js/app.js
@@ -13,4 +13,22 @@ export default class Character {
     this.health = 100;
     this.level = 1;
     
+    levelUp() = {
+        if (this.health <=0) = {
+            throw new Error('нельзя повысить левел умершего')
+        }
+        this.level += 1;
+        this.health = 100;
+        this.attack *= 1.2;
+        this.defence *= 1.2;
+    }
+    
+    damage(points) {
+        if (this.health > 0) {
+            this.health -= points * (1 - this.defence / 100)
+        } else {
+            throw new Error('game over')
+        }
+    }
 }
+
